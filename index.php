132c231c5c6e9ccfd30608dcdac20bcfd124c128
nathan <nathanhalioua@gmail.com>
2022-02-16 10:49:46 +0100

    Devoir des clubs

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+include 'Comparable.php';
+include 'ISport.php';
+include 'Club.php';
+include 'Sport.php';
+include 'SportBallon.php';
+include 'SportRelais.php';
+include "data.php";
+
+echo '<h2>LISTE DES CLUBS</h2><br>';
+foreach ($club as $keyClub => $valueClub){
+    echo "<a href=result.php?id={$keyClub}>{$keyClub} - {$valueClub->getNomClub()} {$valueClub->getNbPoints()}</a><br>";
+}
+
+echo "<br>
+        FORMULAIRE ID CLUB
+      <br>
+      <form method='post' name='formIdClub' action='result.php'>
+        <label for='club-select'></label>        
+            <select name='IdClub' id='club-select'>
+                <option value=''>--Choisir un club--/</option>";
+foreach ($club as $kCLub => $vClub){
+    echo "<option value='".$vClub->getIdClub()."'>".$vClub->getNomClub()."</option>";
+}
+echo "</select>
+            <br>
+        <button type='submit'>Envoie</button>
+      </form>";
